b81df48f448927f246e9fdce1519d32d5531bd1b
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-09-14 20:25:57 +0300

    first done

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+galleryList.insertAdjacentHTML("beforeend", markup(galleryItems));
+galleryList.addEventListener("click", handleClick);
+
+function markup(arr) {
+  return arr
+    .map(
+      ({ preview, description, original }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join("");
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalSrc = event.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${originalSrc}">
+`);
+
+  instance.show();
+}
